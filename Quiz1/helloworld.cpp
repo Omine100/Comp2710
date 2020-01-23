a1ab9e8c5ed158f593ac4d70898cb4435cac9f72
Omine100 <mrb0094@tigermail.auburn.edu>
2020-01-23 17:24:34 -0600

    Initial upload.

---
+++ Quiz1/helloworld.cpp
@@ -0,0 +1,14 @@
+//Matthew Browning
+//Quiz1
+//This code allows for the text to be printed to the user, such as "Hello world".
+//Much help from class
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    cout << "Hello, World!";
+        cout << endl;  
+	 return 0;
+ }
