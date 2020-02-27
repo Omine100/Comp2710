cca6f4808defa296b06e6ca57024a22b09d22a38
Omine100 <mrb0094@tigermail.auburn.edu>
2020-02-27 11:48:59 -0600

    Update to project 3

--- Projects/comp2710_project3_sortingAlgorithm/project3_mrb0094.cpp
+++ Projects/comp2710_project3_sortingAlgorithm/project3_mrb0094.cpp
@@ -9,9 +9,38 @@
 
 #include <fstream>
 #include <iostream>
+#include <string>
 using namespace std;
+
+//FUNCTION DECLARATION: check_file DECLARATION
+bool check_file(string);
+
+//FUNCTION DECLARATION: read_file DECLARATION
+int read_file(int inputArray[], ifstream& inStream);
 
 //METHOD: START OF THE PROJECT 
 int main() {
-    cout << "Start of the project.";
+    //VARIABLE DECLARATION
+    string firstFile;
+    string secondFile;
+    int firstArray[100];
+    int firstArray_size;
+    int secondArray[100];
+    int secondArray_size;
+
+    cout << "*** Welcome to Matthew's sorting program ***\n"
+         << "Enter the first input file name: ";
+    getline(cin, firstFile);
+    cout << "This is the fileName: " << firstFile;
+}
+
+int read_file(string fileName, int array[]) {
+    string line;
+    ifstream file (fileName);
+
+    if (file.is_open()) {
+        while (getline(file, line)) {
+            array[1] == line.toInt();
+        }
+    }
 }
