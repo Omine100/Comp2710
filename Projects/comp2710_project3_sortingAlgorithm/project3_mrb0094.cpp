a3cafb496a18831d07d81b14a73b8d903d19f8e6
Omine100 <54552984+Omine100@users.noreply.github.com>
2020-02-26 22:02:48 -0600

    Created project3.

---
+++ Projects/comp2710_project3_sortingAlgorithm/project3_mrb0094.cpp
@@ -0,0 +1,17 @@
+/*
+ * FILE NAME: project3_mrb0094.cpp
+ * AUTHOR: Matthew Browning
+ * AU ID: mrb0094
+ * ABOUT: This program simulates a duel between three people and calculates odds.
+ * COMPILE: Compile project3_mrb0094.cpp and run.
+ * HELP: Basic information provided by Professor Li and talk through with Ezra Hill from class.
+*/
+
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+//METHOD: START OF THE PROJECT 
+int main() {
+    cout << "Start of the project.";
+}
